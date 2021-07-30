d6d2ab4a0ec632a72e1f8ef0c420b3f2bae05ca7
Methma W <methma-w@Methmas-MacBook-Pro.local>
2021-07-30 09:16:25 +0530

    Add cancellation feature

--- src/components/Room/index.tsx
+++ src/components/Room/index.tsx
@@ -1,28 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import { useStyles } from './styles';
-import Grid from '@material-ui/core/Grid';
-import * as request from '../../utils/requests';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/reducers';
 
 const Room = (props: any) => {
-	const userData: any = useSelector((state: RootState) => state.auth.userData);
 	const classes = useStyles();
-
-	const [ showReserveBtn, setShowReserveBtn ] = useState(false);
-
-	const checkUserHasBookings = async () => {
-		const myBookings: any = await request.getMyBookings(userData.data._id);
-
-		if (myBookings.data) {
-			setShowReserveBtn(false);
-		}
-	};
 
 	return (
 		<Card className={classes.root}>
@@ -43,15 +28,11 @@ const Room = (props: any) => {
 							${props.data.rates[0].rate}
 						</Typography>
 
-						{showReserveBtn && (
+						{props.showReserveBtn && (
 							<Link to={{ pathname: `/reserve`, state: props.data }}>
-								<button className={classes.customBtn} onClick={checkUserHasBookings}>
-									Reserve
-								</button>
+								<button className={classes.customBtn}>Reserve</button>
 							</Link>
 						)}
-
-						{!showReserveBtn && <p className={classes.noreserve}>You can only have 1 reservation at a time</p>}
 					</div>
 				</div>
 			</CardContent>
--- src/pages/Rooms/index.tsx
+++ src/pages/Rooms/index.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Room } from '../../components';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/reducers';
+import * as request from '../../utils/requests';
 
 const Rooms = () => {
 	const checkin = useSelector((state: RootState) => state.dates.selectedStartDate);
 	const checkout = useSelector((state: RootState) => state.dates.selectedEndDate);
 	const rooms = useSelector((state: RootState) => state.rooms.data);
 
+	const userData: any = useSelector((state: RootState) => state.auth.userData);
+
+	const [ showReserveBtn, setShowReserveBtn ] = useState(false);
+
+	const checkUserHasBookings = async () => {
+		const myBookings: any = await request.getMyBookings(userData.data._id);
+
+		console.log('myBookingsmyBookings', myBookings);
+
+		if (myBookings.data?.length > 0) {
+			setShowReserveBtn(false);
+		}
+
+		if (myBookings.data?.length === 0) {
+			setShowReserveBtn(true);
+		}
+	};
+
+	useEffect(() => {
+		checkUserHasBookings();
+	});
+
 	return (
 		<div>
 			{checkin === '' && <p>Please select a checkin date</p>}
 
+			{!showReserveBtn && <p style={{ textAlign: 'center' }}>You can only have 1 reservation at a time!</p>}
+
 			{checkin !== '' &&
 				rooms.map((room: any) => {
-					return <Room key={room._id} data={room} />;
+					return <Room key={room._id} data={room} showReserveBtn={showReserveBtn} />;
 				})}
 		</div>
 	);
